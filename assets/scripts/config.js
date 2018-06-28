c4ac5e9f0f3713f4b1894eebee2123ef3f9ce808
bentoks2016 <nwogbobaret@gmail.com>
2018-06-28 10:12:34 -0400

    User Registration page created
    -user sign up created
    -user sing in created
    -sign out created
    - password change created

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://intense-sands-49161.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const authEvents = require('./auth/events')
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
@@ -8,4 +9,8 @@
 
 $(() => {
   // your JS code goes here
+  $('#signUpForm').on('submit', authEvents.onSignUp)
+  $('#signInForm').on('submit', authEvents.onSignIn)
+  $('#password_change').on('submit', authEvents.onChangePassword)
+  $('#sign-out').on('click', authEvents.onSignOut)
 })
